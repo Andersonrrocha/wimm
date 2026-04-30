fbf65531f28236f85ddfb58f1956270358891911
Anderson Rocha <andersonrocha.rs@live.com>
2026-04-30 18:35:58 -0300

    chore(shared): build package to dist and configure jest

---
+++ packages/shared/jest.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.spec.ts'],
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+}
